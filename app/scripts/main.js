ef20cbe75f7fe421886ba8df6fb561a534bbc65b
Daniel Vivar <jdvivar@gmail.com>
2016-11-18 22:54:28 +0000

    Added more format

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,44 @@
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('hola');
+  var menu = false;
+  var mobile = undefined;
+
+  $('.menu').click(function(){
+    if (menu) {
+      closeMenu();
+    } else {
+      openMenu();
+    }
+  })
+
+  $('.section-link').click(function(){
+    if (mobile) {
+      closeMenu();
+    }
+  });
+
+  function openMenu() {
+    $('nav .section-link').css('display', 'block');
+    menu = !menu;
+  }
+
+  function closeMenu() {
+    $('nav .section-link').css('display', 'none');
+    menu = !menu;
+  }
+
+  // responsive behaviour trigger
+  var mql = window.matchMedia('(max-width: 768px)');
+  mql.addListener(handleOrientationChange);
+
+  // function which executes whenever a orientation change happens
+  function handleOrientationChange(mql) {
+    if (mql.matches) {
+      mobile = true;
+    } else {
+      mobile = false;
+    }
+  };
+
+  handleOrientationChange(mql);
+
 })
